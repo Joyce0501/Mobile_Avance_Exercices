8dc2796833f3ce32cf0aa18ff4c15c3e5241b203
Joyce <1983276@cegepmontpetit.ca>
2022-08-26 11:46:12 -0400

    les container

--- mise_en_page_simple/lib/main.dart
+++ mise_en_page_simple/lib/main.dart
@@ -24,24 +24,27 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      home: MyHomePage(),
+
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  // const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  // // This widget is the home page of your application. It is stateful, meaning
+  // // that it has a State object (defined below) that contains fields that affect
+  // // how it looks.
+  //
+  // // This class is the configuration for the state. It holds the values (in this
+  // // case the title) provided by the parent (in this case the App widget) and
+  // // used by the build method of the State. Fields in a Widget subclass are
+  // // always marked "final".
+  //
+  // final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -73,35 +76,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+
+        // title: Text(widget.title),
+        title: Text("Mise en page simple"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.all(5),
+              height: 200,// Ca prend un container pour les bordures et les marges
+              width: 200,
+              color: Colors.red,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Text('Yo',
+                style : TextStyle(color: Colors.red, fontSize: 20),
             ),
+            Container(
+
+              height: 200,// Ca prend un container pour les bordures et les marges
+              width: 200,
+              color: Colors.black,
+            ),
+          ],
+        ),
+            // const Text(
+            //   'You have pushed the button this many times:',
+            // ),
+
           ],
         ),
       ),
