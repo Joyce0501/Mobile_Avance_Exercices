4251e8028205c521e9c9f4e0a1fd02c0e6402a0f
Joyce <1983276@cegepmontpetit.ca>
2022-08-26 15:21:29 -0400

    suite de la mise en page et creation du projet sur plusieurs containers

--- plein_de_containers/lib/main.dart
+++ plein_de_containers/lib/main.dart
@@ -75,68 +75,41 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
-          Container(
-            margin: EdgeInsets.all(5),
-            width: double.infinity,
-            height: 200,
-            child:  Expanded(
-              child:
-              Row (
-                  children:[
-                    Expanded(
-                      flex: 2,
-                      child:
-                    Container(
-                      color: Colors.red,
-                    ),
-                    ),
-
-                    Expanded(
-                      child:
-                    Row(
-                      children: [
-                        Text(
-                          'Yo',
-                          style: TextStyle(color: Colors.red),
-                        ),
-                        Column(
-                          children: [
-                            Expanded(child: Container(
-                              color: Colors.white,
-                            ),
-                            ),
-                            Expanded(child: Container(
-                              color: Colors.black,
-                            ),
-                            ),
-                          ],
-                        ),
-                      ],
-                      // color: Colors.black,
-                    ),
-                    ),
-                  ]
-              ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-
-          Row(
-            children: [
-              Expanded(
-                child: MaterialButton(
-                  child: Text('Bouton du bas'),
-                  color: Colors.blue,
-                  onPressed: () {  },
-                ),
-              ),
-            ],
-          ),
-        ],
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
