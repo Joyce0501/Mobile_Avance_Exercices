dc444acf556b643fab6728c6a23a999c3b0ad77f
Joyce <1983276@cegepmontpetit.ca>
2022-09-02 15:59:27 -0400

    EXERCICE ORDRE_ALPHA fin

--- ordre_alpha/lib/main.dart
+++ ordre_alpha/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,14 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,19 +42,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+
+  List<String> prenoms = ["Joyce","Celia","Ivan","Jordan","Pierre"];
+  // int indexcourant = prenoms.indexOf(prenoms[index]),
+  //     int indexprochain = prenoms.indexOf(prenoms[index + 1]),
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
     });
   }
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    prenoms.shuffle(Random());
+  }
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -75,41 +71,73 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // body: ListView.builder(
+      //   itemCount: prenoms.length,
+      //   prototypeItem: ListTile(
+      //     title: Text(prenoms.first),
+      //   ),
+      //   itemBuilder: (context, index) {
+      //     return Row(
+      //       children: [
+      //         Expanded(
+      //           child: ListTile(
+      //             title: Text(prenoms[index]),
+      //           ),
+      //         ),
+      //         Expanded(
+      //           child: MaterialButton(
+      //             child: Text('en haut'),
+      //             color: Colors.blue,
+      //             onPressed: () {  },
+      //           ),
+      //         ),
+      //         Expanded(
+      //           child: MaterialButton(
+      //             child: Text('en bas'),
+      //             color: Colors.blue,
+      //             onPressed: () {  },
+      //           ),
+      //         ),
+      //       ],
+      //     );
+      //   },
+      // )
+      body:
+      Center(
+        child: ListView.builder(
+          itemCount: prenoms.length,
+            prototypeItem: ListTile(
+              title: Text(prenoms.first),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+            itemBuilder: (context,index){
+              return ListTile(
+              leading: IconButton(icon: const Icon(Icons.upload_rounded), onPressed: () {
+                if(prenoms.indexOf(prenoms[index]) != 0){
+                  setState((){
+                    int indexcourant = prenoms.indexOf(prenoms[index]);
+                    int indexenhaut = prenoms.indexOf(prenoms[index - 1]);
+                    String wait = prenoms[index-1];
+                    prenoms[indexenhaut] = prenoms[indexcourant];
+                    prenoms[indexcourant] = wait;
+                  });
+                }
+              }),
+              title: Text(prenoms[index]),
+                trailing: IconButton(icon: const Icon(Icons.download_for_offline), onPressed: () {
+                  if(prenoms.indexOf(prenoms[index]) != prenoms.length - 1){
+                    setState((){
+                      int indexcourant = prenoms.indexOf(prenoms[index]);
+                      int indexenbas = prenoms.indexOf(prenoms[index + 1]);
+                      String wait = prenoms[index+1];
+                      prenoms[indexenbas] = prenoms[indexcourant];
+                      prenoms[indexcourant] = wait;
+                    });
+                  }
+                }),
+              );
+            }
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
   }
 }
