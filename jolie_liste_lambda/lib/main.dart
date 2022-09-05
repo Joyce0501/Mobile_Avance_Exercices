93501b125f358fa9bea213bd186c816d05eca3e1
Joyce <1983276@cegepmontpetit.ca>
2022-09-05 01:23:45 -0400

    Affichage dans le terminal

--- jolie_liste_lambda/lib/main.dart
+++ jolie_liste_lambda/lib/main.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
+  // int fibonacci(int n) => n <= 2 ? 1 : fibonacci(n - 2) + fibonacci (n - 1);
+  //
+  //   String output = "";
+  //   String valeur = "";
+  //
+  //   for (int i = 1; i <= 16; ++i) {
+  //     output += fibonacci(i).toString() + ", ";
+  //   }
+  //   print(output + "...");
+
+
+    int n1=0,n2=1,n3;
+    print(n1);
+    print(n2);
+
+    for(int i = 2 ;i <= 16; i++){
+      n3 = n1+n2;
+      print('$n3');
+      n1 = n2;
+      n2 = n3;
+    }
+
 }
 
 class MyApp extends StatelessWidget {
@@ -49,26 +72,35 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String valeur = "";
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      this._finobacci();
     });
+  }
+
+  void _finobacci(){
+    int n1=0,n2=1,n3;
+    print(n1);
+    print(n2);
+
+    for(int i = 2 ;i <= 16; i++){
+      n3 = n1+n2;
+      print('$n3');
+      n1 = n2;
+      n2 = n3;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    void initState() {
+      this._finobacci();
+    }
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -76,40 +108,9 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
